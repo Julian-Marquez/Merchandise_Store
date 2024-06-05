cce7af2f58f411e460bb617a22a4edbcc4dbc934
Julian-Marquez <144375558+Julian-Marquez@users.noreply.github.com>
2024-06-05 09:21:47 -0600

    Create productinfo.js

---
+++ Mercahndise_Store/src/main/webapp/js/productinfo.js
@@ -0,0 +1,27 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
+
+function showImage(index) {
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+showImage(currentIndex);
+/**
+ * 
+ */
